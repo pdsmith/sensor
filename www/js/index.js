02658a86b9c78ff559341c830760e63260a4a00c
Paul Smith <pauls@sccwrp.org>
2014-06-09 09:50:07 -0700

    version.0.2.33

--- www/js/index.js
+++ www/js/index.js
@@ -372,7 +372,7 @@ var app = {
 	  //if(data !== "Initializing SD card...Initializing SD card...initialization done."){
 	  //var dataType = typeof(data);
 	  alert("Data: "+data);
-	    jsonString = data.replace(/|/g, ',');
+	    jsonString = data.replace(/|/g, '');
 	    //var app.SESSIONID = +new Date;
 	    // remove | pipe ending
 	    //var jsonString = '{"result":true,"count":1}';
